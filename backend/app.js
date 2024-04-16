443cf8b2bacb47d37d0217fe7c4f27ee512de9f4
Warren <warrenpope2010@gmail.com>
2024-04-16 23:07:53 +0530

    added log after connecting to express app

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import cors from 'cors'
+import Log from "./helpers/log.js"
 
 export default class App {
     listen(port) {
         this.port = port
-        this.app.listen(this.port, () => console.log(`Connected to port: ${this.port}`))
+        this.app.listen(this.port, () => {
+            console.log(`Connected to port: ${this.port}`)
+            Log.info(`Connected to port: ${this.port}`)
+        })
     }
 
     init() {
