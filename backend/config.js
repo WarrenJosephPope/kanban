259e6410cdf6549bc9a30136bd2e16d261898265
Warren <warrenpope2010@gmail.com>
2024-04-18 02:03:47 +0530

    fixed port

--- backend/config.js
+++ backend/config.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
-export const PORT = process.env.PORT ?? 8000
+export const PORT = process.env.BACKEND_PORT ?? 8000
 export const MONGODB_URI = process.env.MONGODB_URI ?? ""
 export const LOG_PATH = process.env.LOG_PATH ?? "logs/"
 export const JWT_SECRET = process.env.JWT_SECRET ?? ""
