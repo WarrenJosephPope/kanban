a5dcdabb45b33b5513fe4c137955df21f4f73956
Warren <warrenpope2010@gmail.com>
2024-04-19 00:25:34 +0530

    getting base url

--- backend/config.js
+++ backend/config.js
@@ -2,10 +2,10 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
-export const BASE_URL = process.env.BACKEND_BASE_URL ?? "http://localhost:8000"
+export const BASE_URL = process.env.BASE_URL ?? "http://localhost:8000"
 export const RESET_PASSWORD_LINK = process.env.RESET_PASSWORD_LINK ?? "http://localhost:8000/reset-password"
 
-export const PORT = process.env.BACKEND_PORT ?? 8000
+export const PORT = process.env.PORT ?? 8000
 export const MONGODB_URI = process.env.MONGODB_URI ?? ""
 export const LOG_PATH = process.env.LOG_PATH ?? "logs/"
 export const JWT_SECRET = process.env.JWT_SECRET ?? ""
